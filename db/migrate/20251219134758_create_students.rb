82941cdd2f777ae338f7ed0f2c66e5b9ce473649
Zitao Qiu <qiuzitao23@mails.ucas.ac.cn>
2025-12-22 11:32:01 +0000

    fix: RuboCop style violations

--- db/migrate/20251219134758_create_students.rb
+++ db/migrate/20251219134758_create_students.rb
@@ -1,7 +1,7 @@
 class CreateStudents < ActiveRecord::Migration[8.1]
   def change
     create_table :students do |t|
-      t.string :name, null: false, limit:20, comment: '学生姓名'
+      t.string :name, null: false, limit: 20, comment: '学生姓名'
       t.string :email, null: false, comment: '学生邮箱'
       t.string :student_id, null: false, comment: '学号'
 
